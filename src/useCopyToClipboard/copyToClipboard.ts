c753d07654b92366ab0ad993dc59119ef9b43b96
hamedzain <zainh1210@gmail.com>
2022-08-05 11:50:56 +0300

    Copy to clipboard

--- src/useCopyToClipboard/copyToClipboard.ts
+++ src/useCopyToClipboard/copyToClipboard.ts
@@ -1,3 +1,18 @@
 export const copyToClipboard = (text: string): void => {
-    console.log({text})
+    const textArea = document.createElement('textarea')
+    textArea.style.position = 'fixed'
+    textArea.style.top = '0'
+    textArea.style.left = '0'
+    textArea.style.width = '2em'
+    textArea.style.height = '2em'
+    textArea.style.padding = '0'
+    textArea.style.border = 'none'
+    textArea.style.outline = 'none'
+    textArea.style.boxShadow = 'none'
+    textArea.style.background = 'transparent'
+    textArea.value = text
+    document.body.appendChild(textArea)
+    textArea.select()
+    document.execCommand('copy')
+    document.body.removeChild(textArea)
 }
